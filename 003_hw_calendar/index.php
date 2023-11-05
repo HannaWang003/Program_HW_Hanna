fb1ced76d2d355eadb93c4a2bd8fbfe761aaf8d2
HannaWang003 <hannawang003@gmail.com>
2023-11-05 10:29:22 +0800

    修正檔名及關聯

--- 003_hw_calendar/index.php
+++ 003_hw_calendar/index.php
@@ -1,85 +1,143 @@
 <?php
 include('./files/program.php');
-unset($_SESSION['row']);
-unset($_SESSION['PreMDt']);
-unset($_SESSION['month']);
-unset($_SESSION['Emonth']);
-unset($_SESSION['year']);
-unset($_SESSION['week']);
-unset($_SESSION['monthS']);
-// if($_GET['month'] && $_GET['year']){
-//   $_SESSION['month']=$_GET['month'];
-//   $_SESSION['year']=$_GET['year'];
-// }
-// 存入12個月
-for($i=1;$i<=12;$i++){
-  $monthS=strtotime("2023-$i-1");
-  $_SESSION['monthS'][]=date('F',$monthS);
-}
-// 開始設定月曆
-if(isset($_POST['year']) && !empty($_POST['year'])){
-  $_SESSION['month']=$_POST['month'];
-  $_SESSION['year']=$_POST['year'];
-}
-elseif(isset($_POST['month'])){
-  $_SESSION['month']=$_POST['month'];
-  $_SESSION['year']=date("Y");
-}
-elseif(isset($_GET['month']) && isset($_GET['year']) && isset($_GET['noteD'])){
-  $_SESSION['month']=$_GET['month'];
-  $_SESSION['year']=$_GET['year'];
-}
-  elseif(isset($_GET['prev'])){
-    if($_GET['prev']==1){
-      $_SESSION['month']=12;
-      $_SESSION['year']=$_GET['year']-1;
+?>
+<?php 
+if(isset($_GET['month'])){
+// 秒數更新;需改用js實現(11/04記)
+// header("Refresh:1");
+?> 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>☼CALENDAR</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <?php
+    // 變數
+    $row=$_SESSION['row'];
+    $FC=$_SESSION['PreMDt'];
+    $month=$_SESSION['month'];
+    $Emonth=$_SESSION['Emonth'];
+    $year=$_SESSION['year'];
+    ?>
+
+    <div class="container">
+        <!-- 整個版面開始 -->
+        <div class="containerBox">
+            <div class="BoxHeader">
+                <div class="Emonth text-c1"><?=$Emonth?></div>
+
+                <div class="year text-c1"><?=$year?></div>
+                <div class="time text-c1"><?=date("A h:i:s")?></div>
+                <div class="BHButton">
+                    <button><a class="text-bk" href="calendar.php?prev=<?=$month?>&year=<?=$year?>">PREV</a></button>
+                    <button><a class="text-bk" href="calendar.php">||</a></button>
+                    <button><a class="text-bk" href="calendar.php?next=<?=$month?>&year=<?=$year?>">NEXT</a></button>
+                 </div>
+            </div>
+            <div class=DateSelect>
+                                <!-- 加入選擇日期功能 -->
+                                <form action="calendar.php" method="post">
+                                <input type="text" name="year" id="year" onkeyup="value=value.replace(/^(0+)|[^\d]+/g,'')">
+                                <select name="month" id="month">
+                                    <?php
+                                        for($i=0;$i<12;$i++){ 
+                                            if($i+1==$month){
+                                                echo "<option value=".($i+1)." selected>".$_SESSION['monthS'][$i]."</option>";
+                                            } 
+                                            else{                                         
+                                            echo "<option value=".($i+1).">".$_SESSION['monthS'][$i]."</option>";
+                                        } 
+                                        }
+                                    ?>
+                                </select>
+                                <input class="submitB" type="submit" value="⁞⁞⁞">
+                                </form>
+                  <!-- 加入選擇日期功能end -->
+                </div>
+            <div class='containerBoxS'>
+                <div class='every everyW'>
+                    <?php  
+    foreach($_SESSION['week'] as $key => $w){
+        if($key==0 || $key==6){
+            echo "<span class='bg-c3'>".$w."</span>";
+        }
+        else{
+            echo "<span class='bg-c3'>".$w."</span>";
+        }
+    }
+    ?>
+                </div>
+            </div>
+            <div class="containerBoxS">
+                <?php
+    for($j=0;$j < $row; $j++){
+        echo "<div class='every everyD'>";
+        for($i=0;$i<7;$i++){
+            $tmp=(7*$j)+$i;
+            $everydayTime=strtotime("$tmp days",$FC);
+            $everydayM=date("m",$everydayTime);
+            $everyday=date("d",$everydayTime);
+            $every=date("Y-m-d",$everydayTime);
+            if($every==date("Y-m-d")){
+                ?>
+                 <a class="text-bk" href="calendar.php?month=$month&year=$year&noteD=<?=$everydayTime?>">
+                 <span class='today-c1'><?=$everyday?></span>
+                </a>
+                <?php               
+            }
+            elseif($everydayM==$month){
+                ?>
+                <a class="text-bk" href="calendar.php?month=<?=$month?>&year=<?=$year?>&noteD=<?=$everydayTime?>">
+                <span class='bg-c2'><?=$everyday?></span>
+            </a>
+            <?php
+            }
+    
+            else{
+                    echo "<span class='bg-c3'>";
+                    echo "&nbsp&nbsp;&nbsp&nbsp;</span>";
+            }
+    
+        }
+        echo "</div>";
+    }
+    ?>
+            </div>
+        </div>
+        <!-- 加入記事 -->
+        <?php 
+    if(isset($_COOKIE['noteD'])){
+        // ?>
+        <div class="notes bg-c2">
+<!-- <form action="calendar.php?noteD=<?=$_COOKIE['noteD']?>" method="post"> -->
+    <?php echo date("Y-m-d",$_COOKIE['noteD']);?>
+    <!-- <input type="text" name="note" id="note"> -->
+    <!-- <input type="submit" value="OK"> -->
+<!-- </form> -->
+</div>
+        <?php 
     }
     else{
-      $_SESSION['month']=$_GET['prev']-1;
-      $_SESSION['year']=$_GET['year'];
-    }
-  }
-  elseif(isset($_GET['next'])){
-    if($_GET['next']==12){
-      $_SESSION['month']=1;
-      $_SESSION['year']=$_GET['year']+1;
-    }
-    else{
-      $_SESSION['month']=$_GET['next']+1;
-      $_SESSION['year']=$_GET['year'];
+        echo "<div class='notes bg-c2'></div>";
     }
 
-  }
+    // ?>
+                <!-- 加入記事end -->
+        <!-- 整個版面結尾 -->
+    </div>
+   
+</body>
+
+</html>
+<?php
+}
 else{
-    $_SESSION['month']=date('m');
-    $_SESSION['year']=date('Y');  
+    header("location:calendar.php");
 }
-$month=$_SESSION['month'];
-$year=$_SESSION['year'];
-$monthD1=date("{$year}-{$month}-1");
-$monthD1time=strtotime($monthD1);
-$monthD1w=date('w',$monthD1time);
-$_SESSION['PreMDt']=strtotime("-$monthD1w days",$monthD1time);
-$preMonthD=date("Y-m-d",$_SESSION['PreMDt']);
-$monthDend=date("t",strtotime($monthD1));
-$monthDendtime=strtotime("{$year}-{$month}-$monthDend");
-$_SESSION['row']=ceil(($monthD1w+$monthDend)/7);
-$_SESSION['Emonth']=date("M",$monthD1time);
-//first row
-$Mfc=$_SESSION['PreMDt'];
-for($i=0;$i<7;$i++){
-$_SESSION['week'][]=date("D",$Mfc);
-$Mfc=strtotime("+ 1 days" , $Mfc);
-}
-//note
-if(isset($_GET['noteD'])){
-setcookie('noteD',$_GET['noteD'],time()+5);
-setcookie('noteDate',date("Y-m-d",$_COOKIE['noteD']),time()+60*60*24*365);
-}
-if(isset($_POST['note']) && !empty($_POST['note'])){
-setcookie("note",$_POST['note'],time()+60*60*24*365);
-setcookie($_COOKIE['noteDate'],$_COOKIE['note'],time()+60*60*24*365);
-}
-// $_SESSION['week']=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-header("location:calendar.php")
 ?>
