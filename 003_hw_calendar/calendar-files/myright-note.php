00fac4966863b09025ffd89d5f37e751278f225f
Hanna <hannawang003@gmail.com>
2023-11-15 08:57:03 +0800

    update everynote position

--- 003_hw_calendar/calendar-files/myright-note.php
+++ 003_hw_calendar/calendar-files/myright-note.php
@@ -24,7 +24,7 @@
 </div>
 <!-- section_note -->
 <div class="container">
-    <div class="row m-auto justify-content-center">
+    <div class="row m-auto">
         <?php
 if(isset($_COOKIE['note'])){
     foreach($_COOKIE['note'] as $key => $val){
@@ -34,64 +34,71 @@ $limitD=ceil($limitDt/60/60/24);
 if($limitD<0){
         ?>
         <div class="toast show col-12 col-sm-3 m-1 myw-A mytext-c3 mybg-c4 border-secondary">
-  <?php
+            <?php
     }
     elseif($limitD==0){
         ?>
-        <div class="toast show col-12 col-sm-3 m-1 myw-A mytext-c3 mybg-c4 border-danger">
-  <?php
+            <div class="toast show col-12 col-sm-3 m-1 myw-A mytext-c3 mybg-c4 border-danger">
+                <?php
     }
     elseif($limitD==1){
         ?>
-        <div class="toast show col-12 col-sm-3 m-1 myw-A mytext-c3 mybg-c4 border-warning">
-  <?php
+                <div class="toast show col-12 col-sm-3 m-1 myw-A mytext-c3 mybg-c4 border-warning">
+                    <?php
     }
     else{
         ?>
-<div class="toast show col-12 col-sm-3 m-1 myw-A mytext-c3 mybg-c4 border-primary">
-        <?php
+                    <div class="toast show col-12 col-sm-3 m-1 myw-A mytext-c3 mybg-c4 border-primary">
+                        <?php
     }
     ?>
-            <div class="toast-header mybg-c4 border-0">
-                <strong class="me-auto text-light"><?=date("Y ‣ m • d",$key)?></strong>
-                <a class="mytext-c3"
-                    href="calendar.php?month=<?=$month?>&year=<?=$year?>&Ddate=<?=$key?>&Dnote=<?=$val?>"><i
-                        class="fa-solid fa-trash-can fa-bounce fa-xl" style="color:white"></i></a>
-            </div>
-            <div class="toast-body p-1 text-light">
-                <p><?=$val?></p>
-                <?php
+                        <div class="toast-header mybg-c4 border-0">
+                            <strong class="me-auto text-light"><?=date("Y ‣ m • d",$key)?></strong>
+                            <a class="mytext-c3"
+                                href="calendar.php?month=<?=$month?>&year=<?=$year?>&Ddate=<?=$key?>&Dnote=<?=$val?>"><i
+                                    class="fa-solid fa-trash-can fa-bounce fa-xl" style="color:white"></i></a>
+                        </div>
+                        <div class="toast-body p-1 text-light">
+                            <p><?=$val?></p>
+                            <?php
 if($limitD<0){
   ?>
-                <p class="bg-secondary text-light p-2"><i class="fa-regular fa-calendar-xmark fa-xl"></i>&nbsp;overdue X ‣ <?=$limitD?>dyas</p>
-                <?php
+                            <p class="bg-secondary text-light p-2"><i
+                                    class="fa-regular fa-calendar-xmark fa-xl"></i>&nbsp;overdue X ‣ <?=$limitD?>dyas
+                            </p>
+                            <?php
   } 
   elseif($limitD==0){
     ?>
-    <p class="bg-danger text-light p-2"><i class="fa-regular fa-calendar-check fa-xl"></i>&nbsp;Done&nbsp;&nbsp;? ‣‣ <?=$limitD?>dyas</p>
-    <?php
+                            <p class="bg-danger text-light p-2"><i
+                                    class="fa-regular fa-calendar-check fa-xl"></i>&nbsp;Done&nbsp;&nbsp;? ‣‣
+                                <?=$limitD?>dyas</p>
+                            <?php
   }
   elseif($limitD==1){
     ?>
-                <p class="bg-warning text-dark p-2"><i class="fa-regular fa-calendar-minus fa-xl"></i>&nbsp;Deadline ‣ <?=$limitD?>dyas</p>
-                <?php
+                            <p class="bg-warning text-dark p-2"><i
+                                    class="fa-regular fa-calendar-minus fa-xl"></i>&nbsp;Deadline ‣ <?=$limitD?>dyas</p>
+                            <?php
 }
 else{
   ?>
-                <p class="bg-primary text-dark p-2"><i class="fa-regular fa-calendar-plus fa-xl"></i>&nbsp;time limit ‣ <?=$limitD?>dyas</p>
-                <?php
+                            <p class="bg-primary text-dark p-2"><i
+                                    class="fa-regular fa-calendar-plus fa-xl"></i>&nbsp;time limit ‣ <?=$limitD?>dyas
+                            </p>
+                            <?php
 }
 ?>
-            </div>
-        </div>
-        <?php
+                        </div>
+                    </div>
+                    <?php
     }
 }
 ?>
-    </div>
-</div>
+                </div>
+            </div>
 
 
 
 
-<!-- 背景圖片取自:https://lapeacefulday.com/ -->
+            <!-- 背景圖片取自:https://lapeacefulday.com/ -->
