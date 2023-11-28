d9c74d9d63aa33c2b3f36e6e1dad04c5b6567fe7
Hanna <hannawang003@gmail.com>
2023-11-28 15:06:43 +0800

    fix:青蛙跳動的位置

--- 003_hw_calendar/calendar-files/first-in.php
+++ 003_hw_calendar/calendar-files/first-in.php
@@ -28,7 +28,7 @@ body {
     }
 
     12% {
-        transform: translate(12vw, 5vh);
+        transform: translate(12vw, -200%);
     }
 
     25% {
@@ -36,31 +36,31 @@ body {
     }
 
     37% {
-        transform: translate(37vw, 5vh);
+        transform: translate(37vw, -200%);
     }
 
     50% {
-        transform: translate(50vw, 0px);
+        transform: translate(50vw, 0vh);
     }
 
     62% {
-        transform: translate(62vw, 5vh);
+        transform: translate(62vw, -200%);
     }
 
     75% {
-        transform: translate(75vw, 0px);
+        transform: translate(75vw, 0vh);
     }
 
     87% {
-        transform: translate(87vw, 5vh);
+        transform: translate(87vw, -200%);
     }
 
     100% {
-        transform: translate(100vw, 0px);
+        transform: translate(100vw, 0vh);
     }
 }
 
-.fog {
+.forg {
     height: 2.5vh;
     position: absolute;
     bottom: 0;
@@ -68,14 +68,14 @@ body {
 
 }
 
-.fog img {
+.forg img {
     width: 100px;
     animation: jumpRight 5s ease-in-out infinite;
 }
 </style>
 
 <body>
-    <div class="container">
+    <div class="container min-vh-100">
         <div class="row mt-2">
             <button type="button" class="col-12 col-sm m-1 py-2 btn mybtn"><a class="text-dark"
                     href="./calendar.php?your_choice=1"><i class="fa-regular fa-hand-scissors fa-xl"></i></a></button>
@@ -87,21 +87,21 @@ body {
         <div class="row text-center my-1 my-sm-5">
             <div class="col-4 text-light">
                 <h2>You play</h1>
-                    <?=$_SESSION['your_choice'];?>
+                    <?= $_SESSION['your_choice']; ?>
             </div>
             <h1 class="col-4">VS</h1>
             <div class="col-4 text-light">
                 <h2>Opponent plays</h1>
-                    <?=$_SESSION['computer'];?>
+                    <?= $_SESSION['computer']; ?>
             </div>
             <div class="row m-auto flex-column my-sm-3 my-1">
-                <h1 class=""><?=$_SESSION['result'];?></h1>
-                <h1 class="">won&nbsp;<?=$_SESSION['win']?>/<?=$_SESSION['n']?>&nbsp;times</h1>
+                <h1 class=""><?= $_SESSION['result']; ?></h1>
+                <h1 class="">won&nbsp;<?= $_SESSION['win'] ?>/<?= $_SESSION['n'] ?>&nbsp;times</h1>
             </div>
 
 
         </div>
-        <div class="fog">
+        <div class="forg">
             <img class="element-to-animate" src="./img/bg-jump-1.png" alt="">
         </div>
     </div>
